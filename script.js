d650d7c50e21d757a96ba3f82f485189569bfee1
love-web-story <98271910+Kamil-Kadirov@users.noreply.github.com>
2026-06-05 15:06:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ document.addEventListener('mousemove', (e) => {
     }
 });
 
-// СВЕРХСКОРОСТНОЙ ДВИЖОК: Коэффициент 0.35 убирает медлительность и дает моментальный отклик
+// СВЕРХСКОРОСТНОЙ ДВИЖОК: Коэффициент 0.80 убирает медлительность и дает моментальный отклик
 function animateCursor() {
-    cursorX += (mouseX - cursorX) * 0.35; 
-    cursorY += (mouseY - cursorY) * 0.35;
+    cursorX += (mouseX - cursorX) * 0.80; 
+    cursorY += (mouseY - cursorY) * 0.80;
 
     if (cursor && window.innerWidth >= 768) {
         cursor.style.transform = `translate3d(${cursorX}px, ${cursorY}px, 0) translate(-50%, -50%)`;
